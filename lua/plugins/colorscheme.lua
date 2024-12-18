a6a566a43980f934e24ec9e82ef2a7512337cf31
Samuli Välimäki <samuli.valimaki@sitowise.com>
2024-12-18 12:18:49 +0200

    feat: use fzf-lua instead of telescope and colorscheme opts

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,5 +5,17 @@ return {
       colorscheme = "rose-pine-moon",
     },
   },
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        dim_inactive_windows = true,
+        -- styles = {
+        --   transparency = true,
+        -- },
+      })
+    end,
+  },
 }
