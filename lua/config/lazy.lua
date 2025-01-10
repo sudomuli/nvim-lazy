7bba15d2cda47ea7badf900c52d86b51b471082b
Samuli Välimäki <samuli.valimaki@sitowise.com>
2025-01-10 11:53:56 +0200

    feat: replace harpoon with default config lazyextra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,6 +40,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     -- { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
     -- { import = "lazyvim.plugins.extras.editor.telescope" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
 
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,31 +17,4 @@ return {
       })
     end,
   },
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = function()
-      local harpoon = require("harpoon")
-
-      -- REQUIRED
-      harpoon:setup()
-      -- REQUIRED
-
-      vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():add()
-      end)
-      vim.keymap.set("n", "<C-e>", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-S-P>", function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set("n", "<C-S-N>", function()
-        harpoon:list():next()
-      end)
-    end,
-  },
 }
